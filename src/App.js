fd9369da3f58674755168e8a2aa1e21176d81cbd
sunny <saurabh1080i@gmail.com>
2022-03-22 10:04:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-800 flex justify-center items-center flex-col">
+    <p className="text-4xl text-red-400">Hello World!</p>
+    <a href="https://reactjs.org/docs/getting-started.html" target="__blank" className="text-2xl underline text-sky-400 mt-2">Getting Started With React!</a>
+    </div>
+  );
+}
+
+export default App;
